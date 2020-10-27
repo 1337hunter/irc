59325b8f4dd246ea77417e6c1f6eb2292cf88959
salec <salec@student.21-school.ru>
2020-10-27 16:04:51 +0300

    ft_irc splitstring tool const ref update

--- srcs/tools.cpp
+++ srcs/tools.cpp
@@ -6,13 +6,13 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 21:08:41 by salec             #+#    #+#             */
-/*   Updated: 2020/10/27 14:34:48 by salec            ###   ########.fr       */
+/*   Updated: 2020/10/27 16:02:01 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tools.hpp"
 
-t_strvect		ft_splitstring(std::string msg, std::string delim)
+t_strvect		ft_splitstring(std::string msg, std::string const &delim)
 {
 	std::vector<std::string>	split;
 	size_t						pos = 0;
