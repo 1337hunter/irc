bce99b43109dfd369d3f16abf87bd8fb565b91f1
salec <salec@student.21-school.ru>
2020-11-04 20:22:39 +0300

    ft_irc 002-004 try

--- srcs/cmds/cmd_user.cpp
+++ srcs/cmds/cmd_user.cpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 16:30:28 by salec             #+#    #+#             */
-/*   Updated: 2020/11/04 17:23:25 by salec            ###   ########.fr       */
+/*   Updated: 2020/11/04 20:22:20 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,22 @@ void		cmd_user(int fd, const t_strvect &split, IRCserv *_server)
 					" :Welcome to the Internet Relay Network " +
 					it->getnickname() + "!" +
 					it->getnickname() + "@" + _server->hostname + CLRF;
+				reply += ":" + _server->hostname + " ";
+				reply += RPL_YOURHOST;
+				reply += " " + it->getnickname() +
+					" :Your host is " + _server->hostname +
+					", running version " + /*_server->version*/ "0.1" + CLRF;
+				reply += ":" + _server->hostname + " ";
+				reply += RPL_CREATED;
+				reply += " " + it->getnickname() +
+					" :This server was created " + "<date>" + CLRF;
+				reply += ":" + _server->hostname + " ";
+				reply += RPL_MYINFO;
+				reply += " " + it->getnickname() + " :" +
+					_server->hostname + " " +
+					/*_server->version*/ "0.1" + " " +
+					"<available user modes>" + " " +
+					"<available channel modes>" + CLRF;
 			}
 			else
 			{
