b47ea3cf9f746e6a1c4c9be823fca2b5e0699403
salec <salec@student.21-school.ru>
2020-11-11 22:08:43 +0300

    ft_irc fix tabs cmd_tools

--- srcs/cmds/cmd_tools.cpp
+++ srcs/cmds/cmd_tools.cpp
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_tools.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbright <gbright@student.21-school.ru>     +#+  +:+       +#+        */
+/*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 17:08:35 by gbright           #+#    #+#             */
-/*   Updated: 2020/11/11 17:12:21 by gbright          ###   ########.fr       */
+/*   Updated: 2020/11/11 22:06:23 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ircserv.hpp"
 #include "commands.hpp"
 
-std::string     reply_welcome(IRCserv *serv, t_citer it)
+std::string		reply_welcome(IRCserv *serv, t_citer it)
 {
-    std::string reply;
+	std::string	reply;
 
-    reply += ft_buildmsg(serv->hostname, RPL_WELCOME,
-            it->getnickname(), "",
-            "Welcome to the Internet Relay Network " +
-            it->getnickname() + "!" + it->getnickname() + "@" +
-            serv->hostname);
-    reply += ft_buildmsg(serv->hostname, RPL_YOURHOST,
-            it->getnickname(), "", "Your host is " + serv->hostname +
-            ", running version " + /*serv->version*/ "0.1");
-    reply += ft_buildmsg(serv->hostname, RPL_CREATED,
-                it->getnickname(), "", "This server was created <date>");
-    reply += ft_buildmsg(serv->hostname, RPL_MYINFO,
-                it->getnickname(), "", serv->hostname + " " +
-                /*serv->version*/ "0.1" + " " +
-                "<available user modes>" + " " +
-                "<available channel modes>");
-    return reply;
+	reply += ft_buildmsg(serv->hostname, RPL_WELCOME,
+		it->getnickname(), "",
+		"Welcome to the Internet Relay Network " +
+		it->getnickname() + "!" + it->getnickname() + "@" +
+		serv->hostname);
+	reply += ft_buildmsg(serv->hostname, RPL_YOURHOST,
+		it->getnickname(), "", "Your host is " + serv->hostname +
+		", running version " + /*serv->version*/ "0.1");
+	reply += ft_buildmsg(serv->hostname, RPL_CREATED,
+		it->getnickname(), "", "This server was created <date>");
+	reply += ft_buildmsg(serv->hostname, RPL_MYINFO,
+		it->getnickname(), "", serv->hostname + " " +
+		/*serv->version*/ "0.1" + " " +
+		"<available user modes>" + " " +
+		"<available channel modes>");
+	return reply;
 }
