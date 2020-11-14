14b5d6be41434994dcd4146adc9790474a09505f
salec <salec@student.21-school.ru>
2020-11-14 18:45:07 +0300

    ft_irc fixed motd rplcode+

--- includes/common_defines.hpp
+++ includes/common_defines.hpp
@@ -6,13 +6,14 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 18:46:30 by salec             #+#    #+#             */
-/*   Updated: 2020/11/10 13:23:57 by gbright          ###   ########.fr       */
+/*   Updated: 2020/11/14 18:44:58 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COMMON_DEFINES_HPP
 # define COMMON_DEFINES_HPP
 
+# define CRLF		"\r\n"
 # define FD_MAX		1024
 # define FD_FREE	0
 # define FD_CLIENT	1
@@ -23,14 +24,6 @@
 # define BUF_SIZE	512
 
 # define DEBUG_MODE	1
-
-// # define KONVERSATION
-
-# ifndef KONVERSATION
-#  define CRLF		"\r\n"
-# else
-#  define CRLF		"\n"	// konversation sends only \n for some reason
-# endif
 
 typedef struct sockaddr		t_sockaddr;
 typedef struct sockaddr_in	t_sockaddr_in;
