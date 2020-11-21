dea98decc6915a9b0d468456c0b5948def9cfd41
salec <salec@student.21-school.ru>
2020-11-21 18:14:05 +0300

    cmd_motd with target a bit better but not done yet

--- srcs/cmds/cmd_motd.cpp
+++ srcs/cmds/cmd_motd.cpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/21 17:32:05 by salec             #+#    #+#             */
-/*   Updated: 2020/11/21 17:48:03 by salec            ###   ########.fr       */
+/*   Updated: 2020/11/21 18:10:43 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,12 @@ void	cmd_motd(int fd, const t_strvect &split, IRCserv *serv)
 	it = ft_findclientfd(serv->clients.begin(), serv->clients.end(), fd);
 	if (it != serv->clients.end())
 		nick = it->getnickname();
-	if (split.size() < 2)
+	if (split.size() < 2 || (split.size() >= 2 && split[1] == serv->servername))
 		serv->fds[fd].wrbuf += reply_motd(serv, nick);
 	else
 	{
 		// we need to get other server motd if arg supplied
+		// forward this motd message to another server and wait for reply?
 		serv->fds[fd].wrbuf += reply_motd(serv, nick);	// placeholder
 	}
 }
