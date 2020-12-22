f4df5795285b4f6d349b4eef0f4a3578885aa476
Antip003 <ant.sergy@gmail.com>
2020-12-22 14:07:11 +0300

    recover

--- srcs/ircserv.cpp
+++ srcs/ircserv.cpp
@@ -47,6 +47,8 @@ void	RunServer(IRCserv *serv)
 		int readyfds = select(lastfd + 1,
 			&(serv->fdset_read), &(serv->fdset_write),
 			&(serv->fdset_error), &timeout);
+		if (DEBUG_MODE && readyfds == 0)
+			std::cout << "Server select tick (" << TIMEOUTSEC << "s)" << std::endl;
 		if (readyfds < 0)
 			error_exit(SELECT);
 		for (int fd = 0; readyfds > 0 && fd <= lastfd; fd++)
