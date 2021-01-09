8b1aee9c5959edaf57e0b6ceb287d1c5059278f8
salec <salec@student.21-school.ru>
2021-01-09 21:44:06 +0300

    ircbot getting ready

--- bonus/ircbot/ircbot.cpp
+++ bonus/ircbot/ircbot.cpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 16:37:43 by salec             #+#    #+#             */
-/*   Updated: 2021/01/09 19:20:27 by salec            ###   ########.fr       */
+/*   Updated: 2021/01/09 21:43:44 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,20 @@ int		main(int argc, char **argv)
 		res = getweather("Moscow");
 
 	std::cout << "\ngot response:\n" << res << std::endl;
+
+	t_weather	w;
+	std::cout << "\nparsed response:" << std::endl;
+	std::cout << "Country:\t" << w.country << std::endl;
+	std::cout << "Location:\t" << w.location << std::endl;
+	std::cout << "Weather:\t" << w.weather << std::endl;
+	std::cout << "Current:\t" << w.temp_c << "C" << std::endl;
+	std::cout << "Feelslike:\t" << w.feels_c << "C" << std::endl;
+	std::cout << "Min temp:\t" << w.mintemp_c << "C" << std::endl;
+	std::cout << "Max temp:\t" << w.maxtemp_c << "C" << std::endl;
+	std::cout << "Pressure:\t" << w.press_mb << "mb" << std::endl;
+	std::cout << "Pressure:\t" << w.press_mmhg << "mmhg" << std::endl;
+	std::cout << "Wind speed:\t" << w.wind_speed << "m/s " << w.wind_deg << std::endl;
+	std::cout << "Humidity:\t" << w.humidity << "%" << std::endl;
 
 	return (0);
 }
--- bonus/ircbot/ircbot.hpp
+++ bonus/ircbot/ircbot.hpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/09 18:57:38 by salec             #+#    #+#             */
-/*   Updated: 2021/01/09 19:09:09 by salec            ###   ########.fr       */
+/*   Updated: 2021/01/09 21:33:50 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,22 @@
 #define MAX_REQUEST_LEN	1024
 #define BUF_SIZE		8192
 #define CRLF 			"\r\n"
+
+typedef struct	s_weather
+{
+	std::string	location;
+	std::string	country;
+	std::string	weather;
+	int			temp_c;
+	int			feels_c;
+	int			mintemp_c;
+	int			maxtemp_c;
+	int			press_mb;
+	int			press_mmhg;
+	int			wind_speed;
+	int			wind_deg;
+	int			humidity;
+}				t_weather;
 
 void			error_exit(std::string const &s1);
 
