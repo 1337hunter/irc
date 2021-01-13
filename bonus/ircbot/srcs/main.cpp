fd706f9eb8d83b9be252a2fa28665535dbac8dca
salec <salec@student.21-school.ru>
2021-01-13 22:24:33 +0300

    ircbot update

--- bonus/ircbot/srcs/main.cpp
+++ bonus/ircbot/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 21:41:45 by salec             #+#    #+#             */
-/*   Updated: 2021/01/13 21:42:00 by salec            ###   ########.fr       */
+/*   Updated: 2021/01/13 22:24:30 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		main(int argc, char **argv)
 	if (argc > 1)
 		split.push_back(argv[1]);
 	else
-		split.push_back("!weather Moscow");
+		split.push_back("!help");
 
 	ProcessMessage(split, bot);
 
