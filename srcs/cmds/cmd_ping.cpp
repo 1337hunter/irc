0b6eeb9d33d0a9d6e18d1612fb3db3307e44b8be
salec <salec@student.21-school.ru>
2021-01-18 02:50:42 +0300

    ping pong done. need testing

--- srcs/cmds/cmd_ping.cpp
+++ srcs/cmds/cmd_ping.cpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/04 16:35:05 by salec             #+#    #+#             */
-/*   Updated: 2021/01/18 00:27:35 by salec            ###   ########.fr       */
+/*   Updated: 2021/01/18 02:45:07 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,54 @@
 
 void		cmd_ping(int fd, const t_strvect &split, IRCserv *serv)
 {
-	t_fd	&fdref = serv->fds[fd];
+	t_fd		&fdref = serv->fds[fd];
+	int			i = 0;
+	std::string	nick = "";
 
-	if (split.size() < 2)
+	if (fdref.type == FD_UNREGISTRED)
+		return ;
+	if (split[0][0] == ':')
+	{
+		i = 1;
+		nick = split[0].substr(1);
+	}
+	else if (fdref.type == FD_SERVER)
+	{
+		t_server	*server = find_server_by_fd(fd, serv);
+		if (!server)
+			return ;
+		nick = server->servername;
+	}
+	else if (fdref.type == FD_SERVICE)
+	{
+		t_service	*service = find_service_by_fd(fd, serv);
+		if (!service)
+			return ;
+		nick = service->name;
+	}
+	else
+	{
+		Client		*client = find_client_by_fd(fd, serv);
+		if (!client)
+			return ;
+		nick = client->getnick();
+	}
+
+	t_strvect	args(split.begin() + i + 1, split.end());
+	if (args.size() == 0)
 		fdref.wrbuf += ft_buildmsg(serv->servername,
 			ERR_NOORIGIN, "", "", "No origin specified");
-	else if (split.size() == 2)
+	else if (args.size() == 1 || (args.size() >= 2 && args[1] == serv->servername))
 		fdref.wrbuf += ft_buildmsg(serv->servername, "PONG",
-			serv->servername, "", split[1]);
-	else if (split.size() >= 3)
+			serv->servername, "", args[0]);
+	else if (args.size() >= 2)	// then forward to args[1]
 	{
-		// then forward to split[2]
+		t_server	*sendto = find_server_by_name(args[1], serv);
+		if (sendto)
+			serv->fds[sendto->fd].wrbuf += ":" + nick + " PING " +
+				args[0] + " :" + args[1] + CRLF;
+		else
+			fdref.wrbuf += ft_buildmsg(serv->servername,
+				ERR_NOSUCHSERVER, nick, args[1], "No such server");
 	}
 }
--- srcs/cmds/cmd_pong.cpp
+++ srcs/cmds/cmd_pong.cpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 19:46:48 by salec             #+#    #+#             */
-/*   Updated: 2021/01/18 00:44:15 by salec            ###   ########.fr       */
+/*   Updated: 2021/01/18 02:44:55 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,53 @@
 
 void		cmd_pong(int fd, const t_strvect &split, IRCserv *serv)
 {
-	t_fd	&fdref = serv->fds[fd];
+	t_fd		&fdref = serv->fds[fd];
+	int			i = 0;
+	std::string	nick = "";
 
-	if (split.size() < 2)
+	if (fdref.type == FD_UNREGISTRED)
+		return ;
+	if (split[0][0] == ':')
+	{
+		i = 1;
+		nick = split[0].substr(1);
+	}
+
+	t_strvect	args(split.begin() + i + 1, split.end());
+	if (args.size() < 1)
 		fdref.wrbuf += ft_buildmsg(serv->servername,
 			ERR_NOORIGIN, "", "", "No origin specified");
-	else if ((split.size() == 2 && split[1] == serv->servername) ||
-		(split.size() >= 4 && split[3] == serv->servername))
+	else if ((args.size() == 1 && args[0] == serv->servername) ||
+		(args.size() >= 2 && args[1] == serv->servername))
 	{
 		fdref.awaitingpong = false;
 		fdref.lastactive = ft_getcurrenttime();
 	}
-	else if (split.size() >= 3)
+	else if (args.size() >= 2)	// then forward to args[2]
 	{
-		// then forward to split[2]
+		Client		*client = NULL;
+		t_service	*service = NULL;
+		if (nick.empty() && (client = find_client_by_fd(fd, serv)))
+			nick = client->getnick();
+		else if (nick.empty() && (service = find_service_by_fd(fd, serv)))
+			nick = service->name;
+		if (nick.empty() && (fdref.type == FD_CLIENT || fdref.type == FD_OPER ||
+			fdref.type == FD_SERVICE))
+			return ;
+
+		t_server	*sendto = find_server_by_name(args[1], serv);
+		if (sendto)
+			serv->fds[sendto->fd].wrbuf += ":" + nick + " PONG " +
+				args[0] + " :" + args[1] + CRLF;
+		else
+		{
+			client = find_client_by_nick(args[1], serv);
+			if (client)
+				serv->fds[client->getFD()].wrbuf += ":" + nick + " PONG " +
+					args[0] + " :" + args[1] + CRLF;
+			else
+				fdref.wrbuf += ft_buildmsg(serv->servername,
+					ERR_NOSUCHSERVER, nick, args[1], "No such server");
+		}
 	}
 }
