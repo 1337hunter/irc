ac42c98f0a1ba767abd71f56d448e552db2d1bab
salec <salec@student.21-school.ru>
2021-01-18 00:44:52 +0300

    fix ping pong to server

--- srcs/cmds/cmd_pong.cpp
+++ srcs/cmds/cmd_pong.cpp
@@ -6,7 +6,7 @@
 /*   By: salec <salec@student.21-school.ru>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/17 19:46:48 by salec             #+#    #+#             */
-/*   Updated: 2021/01/17 22:56:13 by salec            ###   ########.fr       */
+/*   Updated: 2021/01/18 00:44:15 by salec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void		cmd_pong(int fd, const t_strvect &split, IRCserv *serv)
 	if (split.size() < 2)
 		fdref.wrbuf += ft_buildmsg(serv->servername,
 			ERR_NOORIGIN, "", "", "No origin specified");
-	else if (split.size() == 2 && split[1] == serv->servername)
+	else if ((split.size() == 2 && split[1] == serv->servername) ||
+		(split.size() >= 4 && split[3] == serv->servername))
 	{
 		fdref.awaitingpong = false;
 		fdref.lastactive = ft_getcurrenttime();
